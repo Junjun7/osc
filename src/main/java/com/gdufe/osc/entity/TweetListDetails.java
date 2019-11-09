99d7773826ed5359e52bb46498512b89d2b63e96
changwenbo <changwenbo@xiaomi.com>
2019-11-09 13:40:40 +0800

    晚上测试

--- src/main/java/com/gdufe/osc/entity/TweetListDetails.java
+++ src/main/java/com/gdufe/osc/entity/TweetListDetails.java
@@ -3,6 +3,7 @@ package com.gdufe.osc.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(callSuper = true)
 public class TweetListDetails extends TweetList {
 
 	private List<String> imgBig;
