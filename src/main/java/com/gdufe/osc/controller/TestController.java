6f7770c69c3d0e9cc1214193d43a0a954d3b76d8
changwenbo <changwenbo@xiaomi.com>
2020-02-23 16:01:58 +0800

    图片

--- src/main/java/com/gdufe/osc/controller/TestController.java
+++ src/main/java/com/gdufe/osc/controller/TestController.java
@@ -31,7 +31,9 @@ public class TestController {
 
 	@RequestMapping("/img/url")
 	public String getImgUrl() {
-		return "https://pic4.zhimg.com/50/v2-469b1192443fa2aa7359e92b625bad42_r.jpg";
+		return "https://pic1.zhimg.com/80/v2-09716650530b30759143589279cccd44_hd.jpg";
+		// 二维码和大打赏
+//		return "https://pic4.zhimg.com/50/v2-469b1192443fa2aa7359e92b625bad42_r.jpg";
 	}
 
 	@RequestMapping("/zhihu")
