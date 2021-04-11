87664fb655c3e93bed2519d16eb8854b7afe6180
changwenbo <changwenbo@xiaomi.com>
2021-04-11 16:25:41 +0800

    zk

--- src/main/java/com/gdufe/osc/conf/SentinelAspectConfiguration.java
+++ src/main/java/com/gdufe/osc/conf/SentinelAspectConfiguration.java
@@ -1,14 +1,12 @@
 package com.gdufe.osc.conf;
 
-import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SentinelAspectConfiguration {
 
-	@Bean
-	public SentinelResourceAspect sentinelResourceAspect() {
-		return new SentinelResourceAspect();
-	}
+//	@Bean
+//	public SentinelResourceAspect sentinelResourceAspect() {
+//		return new SentinelResourceAspect();
+//	}
 }
